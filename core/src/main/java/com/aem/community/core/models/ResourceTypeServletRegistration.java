4424f9181e4a4fa5cd7399aa52770db2169622b7
MohanKrishna Nalluri <MohanKrishna.Nalluri@sprint.com>
2018-06-28 13:17:46 -0400

    Created a POC to explaing servlet registration using resource types

---
+++ core/src/main/java/com/aem/community/core/models/ResourceTypeServletRegistration.java
@@ -0,0 +1,81 @@
+package com.aem.community.core.models;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.Servlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.ServletResolverConstants;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.osgi.service.component.annotations.Component;
+
+/***    
+ * 
+ * @author mohan krishna nalluri
+ *
+ *This class explains how to register Servlet using the resourceTypes and by passing selectors you can write different use cases
+ *
+ *This can be triggered from any page that has the given resourceType and can be used to abstract the API calls
+ */
+
+/*
+ Using SCR annotations
+ */
+
+
+//@SlingServlet(
+// methods = {"GET"},
+// metatype= true,
+// resourceTypes= {"boostmobile-shop/components/structure/pdp-page"},
+// selectors= {"one","two"})
+
+
+/*
+ Using R6 annotations
+ */
+@Component(
+        immediate=true,
+        service=Servlet.class,
+           property={
+                   ServletResolverConstants.SLING_SERVLET_METHODS+"=GET",
+                   ServletResolverConstants.SLING_SERVLET_RESOURCE_TYPES+"=AEM63POCS/components/structure/page",
+                   ServletResolverConstants.SLING_SERVLET_SELECTORS+"=one",
+                   ServletResolverConstants.SLING_SERVLET_SELECTORS+"=two",
+                   ServletResolverConstants.SLING_SERVLET_EXTENSIONS+"=html"
+           })
+public class ResourceTypeServletRegistration extends SlingAllMethodsServlet {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+             PrintWriter out = response.getWriter();
+             String a = null;
+             List<String> list = Arrays.asList(request.getRequestPathInfo().getSelectors());
+             if (null != request.getRequestParameter("kittu")) {
+              a = request.getRequestParameter("kittu").getString();
+             }
+             if(list.contains("one")) {
+                 out.println("<html><body>");
+                 out.println("<h1>This value was returned by an AEM Sling Servlet bound by using a Sling ResourceTypes property one</h1>" + a);
+                 out.println("</html></body>");
+             }
+             if(list.contains("two")) {
+                 out.println("<html><body>");
+                 out.println("<h1>This value was returned by an AEM Sling Servlet bound by using a Sling ResourceTypes property two</h1>" + a);
+                 out.println("</html></body>");
+             }
+             if(list.contains("three")) {
+                 out.println("<html><body>");
+                 out.println("<h1>This value was returned by an AEM Sling Servlet bound by using a Sling ResourceTypes property three</h1>" + a);
+                 out.println("</html></body>");
+             }
+             out.println("<html><body>");
+             out.println("<h1>This value was returned by an AEM Sling Servlet bound by using a Sling ResourceTypes property NO</h1>" + a);
+             out.println("</html></body>");
+             
+      }
+    }
